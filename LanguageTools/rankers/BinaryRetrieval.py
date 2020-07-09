8a9e5307e82e2028f404be57b4a0ed6520a13e9c
Vitaly R <mortiv16@gmail.com>
2020-07-09 17:06:58 +0300

    add simple ranker

---
+++ LanguageTools/rankers/BinaryRetrieval.py
@@ -0,0 +1,66 @@
+import os
+import pickle
+from collections import Counter
+
+from LanguageTools.DocumentCorpus import DocumentCorpus
+from LanguageTools.rankers import SimilarityEngine
+from LanguageTools.file_utils import check_dir_exists
+
+
+class BinaryRetrieval(SimilarityEngine):
+    def __init__(self, corpus: DocumentCorpus):
+        if not isinstance(corpus, DocumentCorpus):
+            raise TypeError("courpus should be an instance of DocumentCorpus")
+
+        super(BinaryRetrieval, self).__init__(corpus=corpus)
+
+        self.inv_index = None
+        self.build_index()
+
+    def build_index(self):
+        # TODO
+        # include ngrams
+        self.inv_index = dict()
+
+        for id_, doc in self.corpus.corpus:
+            for token in doc:
+                assert token.id is not None, "token id is None, fatal error"
+                if token.id not in self.inv_index:
+                    self.inv_index[token.id] = set()
+                self.inv_index[token.id].add(id_)
+
+    def retrieve_sub_rank(self, tokens):
+
+        doc_ranks = Counter()
+
+        for t_id, rank in tokens:
+            for doc_id in self.inv_index[t_id]:
+                if doc_id not in doc_ranks:
+                    doc_ranks[doc_id] = 0.
+                doc_ranks[doc_id] += rank
+
+        return doc_ranks
+
+    def retrieve_sub(self, tokens):
+        # this does not work with query expansion
+
+        doc_ranks = Counter()
+        for doc_id in set.intersection(*[self.inv_index[t_id] for t_id, rank in tokens]):
+            doc_ranks[doc_id] = 1.
+
+        return doc_ranks
+
+    def save(self, path):
+        check_dir_exists(path)
+
+        pickle.dump(self.inv_index, open(os.path.join(path, "inv_index"), "wb"))
+        pickle.dump(self.corpus.path, open(os.path.join(path, "corpus_ref"), "wb"))
+
+    @classmethod
+    def load(cls, path):
+        corpus = DocumentCorpus.load(pickle.load(open(os.path.join(path, "corpus_ref"), "rb")))
+        retr = BinaryRetrieval(corpus)
+        retr.inv_index = pickle.load(open(os.path.join(path, "inv_index"), "rb"))
+        return retr
+
+
---
+++ LanguageTools/rankers/SimilarityEngine.py
@@ -0,0 +1,60 @@
+from collections.abc import Iterable
+from LanguageTools.DocumentCorpus import DocumentCorpus
+from collections import Counter
+
+
+
+class SimilarityEngine:
+    tok = None
+    corpus = None
+
+    def __init__(self, corpus: DocumentCorpus):
+        self.corpus = corpus
+
+    def query(self, q, exact=False, do_expansion=False, spellcheck=False):
+        if isinstance(q, str):
+            token_ids = [(self.corpus.corpus.vocab[t.text], self.corpus.importance(t.text)) for t in self.corpus.corpus.tok.token_gen(q)]
+        if isinstance(q, Iterable): # may be this is not what i think
+            pass
+        else:
+            raise TypeError("Type is not understood:", type(q))
+
+        if spellcheck:
+            pass # generate several candidate queries
+
+        if not exact and do_expansion :
+            token_ids = self.query_expansion(token_ids)
+
+        # spellchecker should produce several correction candidates (or maybe one)
+        # here need to retrieve both queries and rank which query was more likely
+        relevant_docs = self.retrieve(self.retrieve_sub(token_ids)if exact else self.retrieve_sub_rank(token_ids))
+        return relevant_docs
+
+    def query_expansion(self, tokens):
+        pass # add extra words
+        return tokens
+
+    def retrieve(self, sub_doc_ranks):
+
+        doc_ranks = Counter()
+
+        for sub_doc_id, rank in sub_doc_ranks.items():
+            doc_id = self.corpus.sent_to_doc[sub_doc_id]
+            if doc_id not in doc_ranks:
+                doc_ranks[doc_id] = 0.
+            doc_ranks[doc_id] += rank
+
+        return doc_ranks
+
+    def retrieve_sub(self, token_ids):
+        return {}
+
+    def retrieve_sub_rank(self, tokens):
+        pass
+
+    def save(self):
+        pass # not implemented
+
+    @classmethod
+    def load(cls):
+        pass # not implemented
