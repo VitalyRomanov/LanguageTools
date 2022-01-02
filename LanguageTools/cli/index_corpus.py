07cb758542e6c1709cd3141a62575d3c7a714798
Vitaly R <mortiv16@gmail.com>
2022-01-02 10:18:29 +0300

    update

--- LanguageTools/cli/index_corpus.py
+++ LanguageTools/cli/index_corpus.py
@@ -1,7 +1,10 @@
 import argparse
 
+from tqdm import tqdm
+
 from LanguageTools.corpus import DocumentCorpus
 from LanguageTools.rankers import BinaryRetrieval
+from LanguageTools.rankers.BinaryRetrieval import BinaryRetrievalBiword
 
 
 def main():
@@ -11,14 +14,15 @@ def main():
     args = parser.parse_args()
 
     corpus = DocumentCorpus(args.corpus_path, "en")
+    # corpus = DocumentCorpus.load(args.corpus_path)
 
     with open(args.data_path, "r") as data:
-        for line in data:
+        for line in tqdm(data):
             corpus.add_docs([line], save_instantly=False)
 
     corpus.save()
 
-    ranker = BinaryRetrieval(corpus, index_instantly=True)
+    ranker = BinaryRetrieval(corpus, index_instantly=True, add_bigrams=True)
     ranker.save()
 
 
