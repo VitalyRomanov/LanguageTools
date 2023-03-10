dc49ebba2b3c481a3326b04f8b273585b67bc601
VitalyRomanov <mortiv16@gmail.com>
2023-03-10 12:40:16 +0900

    fix bug

--- LanguageTools/tokenizers/utils.py
+++ LanguageTools/tokenizers/utils.py
@@ -15,5 +15,18 @@ def deaccent(text):
         # assume utf8 for byte strings, use default (strict) error handling
         text = text.decode('utf8')
     norm = unicodedata.normalize("NFD", text)
-    result = u('').join(ch for ch in norm if unicodedata.category(ch) != 'Mn')
+    result = "".join(ch for ch in norm if unicodedata.category(ch) != 'Mn')
     return unicodedata.normalize("NFC", result)
+
+
+def test_deaccent():
+    text = """
+    aulón"
+    Агноёстици́зм (от др.-греч. ἄγνωστος — непознанный).
+    """
+    deacc = deaccent(text)
+    assert deacc == '\n    aulon"\n    Агноестицизм (от др.-греч. αγνωστος — непознанныи).\n    '
+
+
+if __name__ == "__main__":
+    test_deaccent()
