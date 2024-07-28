27c6327e5144a07412113c544a08486b30ae4b17
VitalyRomanov <mortiv16@gmail.com>
2024-07-28 22:56:35 +0200

    updated setup and gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='LanguageTools',
-      version='1.0',
+setup(
+      name='LanguageTools',
+      version='0.0.1',
       py_modules=['LanguageTools'],
       install_requires=[
             "numpy",
@@ -12,5 +13,6 @@ setup(name='LanguageTools',
             "nltk",
             "more_itertools"
       ],
+      packages=["LanguageTools"],
       ext_modules=cythonize("LanguageTools/utils/extra_utils.pyx"),
-      )
+)
